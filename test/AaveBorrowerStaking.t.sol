e28ae9ce50a9b8d78bebf77f14345220f9e9103a
turupawn <ahmed.hn.43@gmail.com>
2023-08-23 18:49:00 -0300

    now working on scroll

--- test/AaveBorrowerStaking.t.sol
+++ test/AaveBorrowerStaking.t.sol
@@ -7,26 +7,40 @@ import "../src/AaveBorrowerStaking.sol";
 contract AaveBorrowerTest is Test {
     AaveBorrowerStaking public aaveBorrowerStaking;
     address AAVE_POOL = 0x48914C788295b5db23aF2b5F0B3BE775C4eA9440;
-    address GHO_TOKEN = 0x5300000000000000000000000000000000000004;
-    address GHO_WHALE = 0x5278Bd8fbB6a3a63E26aDaf8e79866dc7dB4C434;
+    address DAI_TOKEN = 0x7984E363c38b590bB4CA35aEd5133Ef2c6619C40;
+    address DAI_WHALE = 0x707e55a12557E89915D121932F83dEeEf09E5d70;
 
     function setUp() public {
         aaveBorrowerStaking = new AaveBorrowerStaking(
             AAVE_POOL,
-            GHO_TOKEN
+            DAI_TOKEN
         );
+        vm.startPrank(DAI_WHALE);
+        IERC20(DAI_TOKEN).approve(address(AAVE_POOL), 10 ether);
+        IPool(AAVE_POOL).supply(
+            DAI_TOKEN,
+            10 ether,
+            DAI_WHALE,
+            0);
+        IPool(AAVE_POOL).borrow(
+            DAI_TOKEN,
+            0.1 ether,
+            2,
+            0,
+            DAI_WHALE);
+        vm.stopPrank();
     }
 
     function testBorrowAndWitdraw() public {
-        vm.startPrank(GHO_WHALE);
-        IERC20(GHO_TOKEN).approve(address(aaveBorrowerStaking), 0.1 ether);
-        aaveBorrowerStaking.stake(0.1 ether);
+        vm.startPrank(DAI_WHALE);
+        IERC20(DAI_TOKEN).approve(address(aaveBorrowerStaking), 10 ether);
+        aaveBorrowerStaking.stake(10 ether);
 
         skip(50 days);
 
-        aaveBorrowerStaking.unstake(0.1 ether);
+        aaveBorrowerStaking.unstake(10 ether);
         vm.stopPrank();
 
-        aaveBorrowerStaking.withdraw(0.000001 ether);
+        aaveBorrowerStaking.withdraw(0.000000001 ether);
     }
 }
