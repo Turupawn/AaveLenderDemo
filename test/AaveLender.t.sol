9ee343ee386a406f3e3e312b18c2ef1290b9edb6
turupawn <ahmed.hn.43@gmail.com>
2023-08-24 20:55:03 -0300

    improved the test script

--- test/AaveLender.t.sol
+++ test/AaveLender.t.sol
@@ -6,41 +6,35 @@ import "../src/AaveLender.sol";
 
 contract AaveLenderTest is Test {
     AaveLender public aaveLender;
+    // The following addresses apply for Scroll Sepolia, change them depending on the network you're using
     address AAVE_POOL = 0x48914C788295b5db23aF2b5F0B3BE775C4eA9440;
     address DAI_TOKEN = 0x7984E363c38b590bB4CA35aEd5133Ef2c6619C40;
-    address DAI_WHALE = 0x707e55a12557E89915D121932F83dEeEf09E5d70;
+    address DAI_HOLDER = 0x707e55a12557E89915D121932F83dEeEf09E5d70;
 
     function setUp() public {
+        // Start by deploying the AaveLender contract by passing the Aave Pool contract and the token to be lended, DAI in this case
         aaveLender = new AaveLender(
             AAVE_POOL,
             DAI_TOKEN
         );
-        vm.startPrank(DAI_WHALE);
-        IERC20(DAI_TOKEN).approve(address(AAVE_POOL), 10 ether);
-        IPool(AAVE_POOL).supply(
-            DAI_TOKEN,
-            10 ether,
-            DAI_WHALE,
-            0);
-        IPool(AAVE_POOL).borrow(
-            DAI_TOKEN,
-            0.1 ether,
-            2,
-            0,
-            DAI_WHALE);
-        vm.stopPrank();
     }
 
     function testEarnYield() public {
-        vm.startPrank(DAI_WHALE);
+        // The DAI_HOLDER account already has testnet DAI on Scroll Sepolia, let's impersonify it
+        vm.startPrank(DAI_HOLDER);
+
+        // Remember to approve before staking the DAI
         IERC20(DAI_TOKEN).approve(address(aaveLender), 10 ether);
         aaveLender.stake(10 ether);
 
+        // Let's travel to 50 days to the future to earn some DAI
         skip(50 days);
 
+        // The staker should be able to unstake what was originally staked
         aaveLender.unstake(10 ether);
         vm.stopPrank();
 
+        // The owner should be able to earn some DAI during the process
         aaveLender.withdraw(0.000000001 ether);
     }
 }
